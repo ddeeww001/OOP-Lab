243383a81d4aa8e8eb6e940ad190105be40d0dbf
Thitirat Sirisawad <thitirat_sirisawad@cmu.ac.th>
2026-02-26 09:18:42 +0700

    EBook / PhysicalBook

---
+++ src/com/library/lab04/Member.java
@@ -0,0 +1,34 @@
+package com.library.lab04;
+
+public class Member {
+    private String memberId;
+    private String name;
+    private int borrowedCount;
+
+    public Member(String memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+        this.borrowedCount = 0;
+    }
+
+    public String getName() { return name; }
+    public int getBorrowedCount() { return borrowedCount; }
+    public void borrowItem() { this.borrowedCount++; }
+    public void returnItem() { this.borrowedCount--; }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBorrowedCount(int borrowedCount) {
+        this.borrowedCount = borrowedCount;
+    }
+}
